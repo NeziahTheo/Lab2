f8ce4b6cce104b4dab2ea121378f4cd577cbba32
Neziah <neziaht.23@ichat.sp.edu.sg>
2024-05-22 19:59:32 +0800

    Modified Lab 2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,95 @@
-print("ET0735 (DevOps for AIOT) - Lab 2 - Introduction to Python")
+#print("ET0735 (DevOps for AIOT) - Lab 2 - Introduction to Python")
+
+# def funcName (parameter 1, parameter 2) : 
+ #   print("This is a dummy function")
+ #   return 10
+'''
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g 5, 67, 32)")
+
+def get_user_input():
+    print("get_user_input")
+    user_input = input()
+    string_list = user_input.split(",")
+    global float_list
+    float_list = list(map(float, string_list))
+    return float_list
+
+def calc_average():
+    print("calc_average")
+    x = len(float_list)
+    y = sum(float_list)
+    return y/x
+
+def find_min_max():
+    print("find_Min_max")
+    mini = min(float_list)
+    maxi = max(float_list)
+    min_max = [mini, maxi]
+    return min_max
+
+def sort_temperature():
+    print("sort_temperature")
+    float_list.sort()
+    return float_list
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+    float_list.sort()
+    size = len(float_list)
+    if size % 2 :
+     return float_list[size//2]
+    else :
+     return (float_list[size//2] + float_list[1+ size//2]) / 2
+    '''
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas (e.g 5, 67, 32)")
+
+def get_user_input():
+    print("get_user_input")
+    user_input = input()
+    string_list = user_input.split(",")
+    global float_list
+    float_list = list(map(float, string_list))
+    return float_list
+
+def calc_average():
+    print("calc_average")
+    x = len(float_list)
+    y = sum(float_list)
+    return y/x
+
+def find_min_max():
+    print("find_Min_max")
+    mini = min(float_list)
+    maxi = max(float_list)
+    min_max = [mini, maxi]
+    return min_max
+
+def sort_temperature():
+    print("sort_temperature")
+    float_list.sort()
+    return float_list
+
+def calc_median_temperature():
+    print("calc_median_temperature")
+    float_list.sort()
+    size = len(float_list)
+    if size % 2 :
+     return float_list[size//2]
+    else :
+     return (float_list[size//2] + float_list[1+ size//2]) / 2
+#Main code
+def main():
+    display_main_menu()
+    get_user_input()
+    print(calc_average())
+    print(find_min_max())
+    print(sort_temperature())
+    print(calc_median_temperature())
+
+
+if __name__ == "__main__":
+    main()
